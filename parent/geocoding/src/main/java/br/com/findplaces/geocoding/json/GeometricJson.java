5ca2bb49ba30def3e97a4a149356ed2c2d948168
Coradini <mateuscoradini@gmail.com>
2013-08-14 00:40:04 -0300

    Geocode

---
+++ parent/geocoding/src/main/java/br/com/findplaces/geocoding/json/GeometricJson.java
@@ -0,0 +1,11 @@
+package br.com.findplaces.geocoding.json;
+
+import com.google.gson.JsonObject;
+
+public class GeometricJson {
+	
+	public void get(){
+		JsonObject obj = new JsonObject();
+	}
+
+}
