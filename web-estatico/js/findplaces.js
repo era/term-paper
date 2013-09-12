96dbd8ed48d7e9abb53354980884a2998f4225c4
Elias Granja <elias@cit008197.cit>
2013-09-12 10:27:00 -0300

    Added static files

---
+++ web-estatico/js/findplaces.js
@@ -0,0 +1,31 @@
+﻿$.consultaCidade = function (ufSigla, target) {
+    $(target).empty();
+    $.each(uf_cidade.estados, function (u, uf) {
+        if (ufSigla === uf.sigla) {
+            $.each(uf.cidades, function (c, cidades) {
+                $(target).append('<option value="' + cidades + '">' + cidades + '</option>');
+            });
+        }
+    });
+};
+
+$.consultaMapa = function (lat, lng) {
+    $('#map > div').remove();
+
+    var map = new OpenLayers.Map("map");
+    var layer = new OpenLayers.Layer.OSM("Simple OSM Map");
+    var vector = new OpenLayers.Layer.Vector('vector');
+    var controls = map.getControlsByClass('OpenLayers.Control.Navigation');
+
+    for (var i = 0; i < controls.length; ++i)
+        controls[i].disableZoomWheel();
+
+    map.addLayers([layer, vector]);
+
+    map.setCenter(
+        new OpenLayers.LonLat(lng, lat).transform(
+            new OpenLayers.Projection("EPSG:4326"),
+            map.getProjectionObject()
+        ), 15
+    );
+};
