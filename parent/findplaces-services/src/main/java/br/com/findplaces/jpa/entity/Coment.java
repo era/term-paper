91d32c8dac0e8a5fd06d891f4c9962c0e279f7cd
Coradini <mateuscoradini@gmail.com>
2013-10-23 00:08:29 -0200

    FIX Elias ¬¬ (haha copiei) Erro ao subir Jboss

--- parent/findplaces-services/src/main/java/br/com/findplaces/jpa/entity/Coment.java
+++ parent/findplaces-services/src/main/java/br/com/findplaces/jpa/entity/Coment.java
@@ -4,6 +4,9 @@ import java.io.Serializable;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.persistence.ManyToOne;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -16,6 +19,8 @@ public class Coment extends BaseEntity implements Serializable {
 	private static final long serialVersionUID = -3227586023644898530L;
 	
 	@Column
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
 	
 	@OneToOne
